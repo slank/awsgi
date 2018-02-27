7fb89e294fba3b2717ebd8bd614a0f7a7d4d02bd
Gabriel Falcão <gabrielfalcao@users.noreply.github.com>
2018-02-26 22:52:59 -0300

    adds "isBase64Encoded" to APIGateway response
    This solves the error `Malformed Lambda proxy response`
    as per https://aws.amazon.com/premiumsupport/knowledge-center/malformed-502-api-gateway/

--- awsgi/__init__.py
+++ awsgi/__init__.py
@@ -35,6 +35,7 @@ class StartResponse:
 
     def response(self, output):
         return {
+            'isBase64Encoded': False,
             'statusCode': str(self.status),
             'headers': dict(self.headers),
             'body': self.body.getvalue() + ''.join(map(convert_str, output)),
