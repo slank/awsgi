d81efa1c817433eb4a79f7dfd196901b4a2de77d
Matthew Thompson <matt.thompson@topbox.io>
2019-02-12 11:07:38 -0600

    AWS SAM expects an integer status code

--- awsgi/__init__.py
+++ awsgi/__init__.py
@@ -36,7 +36,7 @@ class StartResponse:
     def response(self, output):
         return {
             'isBase64Encoded': False,
-            'statusCode': str(self.status),
+            'statusCode': int(self.status),
             'headers': dict(self.headers),
             'body': self.body.getvalue() + ''.join(map(convert_str, output)),
         }
