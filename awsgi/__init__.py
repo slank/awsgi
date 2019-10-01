1198031d34c37a6a7576f10f11152e4f12261a52
Michiel van Baak <michiel@vanbaak.info>
2019-10-01 13:35:18 +0200

    Make sure to import a good BytesIO implementation in python 2

--- awsgi/__init__.py
+++ awsgi/__init__.py
@@ -1,4 +1,3 @@
-from io import BytesIO
 from base64 import b64encode, b64decode
 import itertools
 import collections
@@ -6,6 +5,7 @@ import sys
 try:
     # Python 3
     from urllib.parse import urlencode
+    from io import BytesIO
 
     # Convert bytes to str, if required
     def convert_str(s):
@@ -13,6 +13,7 @@ try:
 except ImportError:
     # Python 2
     from urllib import urlencode
+    from cStringIO import StringIO as BytesIO
 
     # No conversion required
     def convert_str(s):
